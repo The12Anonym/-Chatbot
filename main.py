4c6a6135f28fc02baed12ecee0a60fce651d5874
JebBenena <a8h81@hotmail.com>
2022-10-16 12:05:05 +0200

    API Update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 prompt = "What is your name?"
 
