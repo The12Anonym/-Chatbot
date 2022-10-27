fbcd16a075c7b62da402e203a9e8f6fb2ba9777e
Luc Bärtschi <baertluc@students.zhaw.ch>
2022-10-27 16:20:41 +0200

    added console app functionality

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import openai
+import constants
+import ai
 
-openai.api_key = "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"
+openai.api_key = constants.OPEN_API_KEY
+name = constants.NAME
+prompt = ""
 
-prompt = "What is your name?"
+print("Hello my name is " + name)
+print ("Please ask me anything")
+
+
+while (prompt != "exit"):
+    prompt = input()
+    openAiObject = ai.openApiCall(prompt)
+    print(ai.getText(openAiObject))
 
 response = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=6)
 
 print(response)
-
-#Test
