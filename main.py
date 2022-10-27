85692809a001f33997b8fca017c755ebcbd3173b
Luc Bärtschi <baertluc@students.zhaw.ch>
2022-10-27 16:30:27 +0200

    fiexd small changes and added contants.py to gitignore

--- main.py
+++ main.py
@@ -7,14 +7,9 @@ name = constants.NAME
 prompt = ""
 
 print("Hello my name is " + name)
-print ("Please ask me anything")
-
+print("Please ask me anything")
 
 while (prompt != "exit"):
     prompt = input()
     openAiObject = ai.openApiCall(prompt)
     print(ai.getText(openAiObject))
-
-response = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=6)
-
-print(response)
