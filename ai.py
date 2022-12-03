76b451ae0557ec23ab7766596b13a1b6799e100d
Luc Bärtschi <luc.baertschi@somnitec.ch>
2022-12-03 13:44:30 +0100

    modified ai.py

--- ai.py
+++ ai.py
@@ -2,7 +2,7 @@ import openai
 #import json
 
 def openApiCall(prompt):
-    response = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=6)
+    response = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=100)
     return response
 
 def getText(openAiObject):
@@ -10,6 +10,7 @@ def getText(openAiObject):
         text = openAiObject.choices[0].text
     else:
         #find best choice 
+        print("Number of answers:" + str(len(openAiObject.choices)))
         text = openAiObject.choices[0].text
 
     return text
